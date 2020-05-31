a7abc8a75a0f3f295d1f9710b53fec22cc1cb2de
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-31 00:26:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < Sinatra::Base
     if @user.username.empty? || !@user.save
       redirect '/failure'
     else
-      @user.save
       redirect '/login'
     end
   end
